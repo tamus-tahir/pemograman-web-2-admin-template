f5f45f655c8023624a9a4d273d5a4bfd6e3ac716
Tamus Tahir <63391521+tamus-tahir@users.noreply.github.com>
2023-06-26 07:44:50 +0800

    Update Auth

--- app/Views/auth/index.php
+++ app/Views/auth/index.php
@@ -18,14 +18,14 @@
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    <!-- Vendor CSS Files -->
-    <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="/assets/vendor/quill/quill.snow.css" rel="stylesheet">
-    <link href="/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-    <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+    <!-- plugins CSS Files -->
+    <link href="/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/plugins/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="/assets/plugins/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="/assets/plugins/quill/quill.snow.css" rel="stylesheet">
+    <link href="/assets/plugins/quill/quill.bubble.css" rel="stylesheet">
+    <link href="/assets/plugins/remixicon/remixicon.css" rel="stylesheet">
+    <link href="/assets/plugins/simple-datatables/style.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
     <link href="/assets/css/style.css" rel="stylesheet">
@@ -112,11 +112,11 @@
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Vendor JS Files -->
-    <script src="/assets/vendor/jquery/jquery-3.7.0.min.js"></script>
-    <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/vendor/parsley/parsley.min.js"></script>
-    <script src="/assets/vendor/sweetalert2/sweetalert2@11"></script>
+    <!-- plugins JS Files -->
+    <script src="/assets/plugins/jquery/jquery-3.7.0.min.js"></script>
+    <script src="/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/plugins/parsley/parsley.min.js"></script>
+    <script src="/assets/plugins/sweetalert2/sweetalert2@11"></script>
     <!-- Template Main JS File -->
     <script src="/assets/js/main.js"></script>
     <script src="/assets/js/script.js"></script>
